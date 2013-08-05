a1c9947aeb64b164f9c8743aa53958b36861110c
MiniApp <suhao@55317.com.cn>
2013-08-05 21:06:52 +0800

    a

---
+++ trunk/source/entity/im/shs/po/User.java
@@ -0,0 +1,65 @@
+package im.shs.po;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * User entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "user", catalog = "webims")
+public class User implements java.io.Serializable {
+
+	// Fields
+
+	private Integer id;
+	private String name;
+	private Integer age;
+
+	// Constructors
+
+	/** default constructor */
+	public User() {
+	}
+
+	/** full constructor */
+	public User(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(name = "name", length = 20)
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "age")
+	public Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
